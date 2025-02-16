9c79c86b0423e895e20207a467e483ff52b13ce1
Oleksandr <sashadovbnya1@gmail.com>
2025-02-16 18:29:07 +0200

    Added tests.

---
+++ HolidaysAPI.Tests/HolidayServiceTests.cs
@@ -0,0 +1,99 @@
+using HolidaysAPI.DB.Repositories;
+using HolidaysAPI.Enums;
+using HolidaysAPI.Models;
+using HolidaysAPI.Services;
+using HolidaysAPI.Services.EnricoAPI;
+using HolidaysAPI.Services.EnricoAPI.Models;
+using Moq;
+
+namespace HolidaysAPI.Tests
+{
+    public class HolidayServiceTests
+    {
+        private readonly Mock<IHolidayRepository> _mockHolidayRepository;
+        private readonly Mock<IEnricoApiConnector> _mockEnricoConnector;
+        private readonly HolidayService _holidayService;
+
+        public HolidayServiceTests()
+        {
+            _mockHolidayRepository = new Mock<IHolidayRepository>();
+            _mockEnricoConnector = new Mock<IEnricoApiConnector>();
+            _holidayService = new HolidayService(_mockEnricoConnector.Object, _mockHolidayRepository.Object);
+        }
+
+        [Fact]
+        public async Task GetDateStatusAsync_ShouldReturnHoliday_WhenDateIsStoredAsHoliday()
+        {
+            var date = new DateTime(2025, 1, 1);
+            _mockHolidayRepository.Setup(repo => repo.GetHolidaysByDateAsync(date))
+                                  .ReturnsAsync(new List<Holiday> { new Holiday { Date = date } });
+
+            var result = await _holidayService.GetDateStatusAsync(date);
+
+            Assert.Equal(DayStatus.Holiday, result);
+        }
+
+        [Fact]
+        public async Task GetDateStatusAsync_ShouldReturnHoliday_WhenEnricoApiReturnsPublicHoliday()
+        {
+            var date = new DateTime(2025, 1, 1);
+            _mockHolidayRepository.Setup(repo => repo.GetHolidaysByDateAsync(date))
+                                  .ReturnsAsync(new List<Holiday>());
+
+            _mockEnricoConnector.Setup(connector => connector.WhereIsPublicHolidayAsync(date))
+                                .ReturnsAsync(new List<CountryModel> { new CountryModel { CountryCode = "us" } });
+
+            var result = await _holidayService.GetDateStatusAsync(date);
+
+            Assert.Equal(DayStatus.Holiday, result);
+        }
+
+        [Fact]
+        public async Task GetDateStatusAsync_ShouldReturnFreeDay_WhenDateIsWeekend()
+        {
+            var date = new DateTime(2025, 1, 4);
+            _mockHolidayRepository.Setup(repo => repo.GetHolidaysByDateAsync(date))
+                                  .ReturnsAsync(new List<Holiday>());
+
+            _mockEnricoConnector.Setup(connector => connector.WhereIsPublicHolidayAsync(date))
+                                .ReturnsAsync(new List<CountryModel>());
+
+            var result = await _holidayService.GetDateStatusAsync(date);
+
+            Assert.Equal(DayStatus.FreeDay, result);
+        }
+
+        [Fact]
+        public async Task GetDateStatusAsync_ShouldReturnWorkDay_WhenDateIsNotHolidayOrFreeDay()
+        {
+            var date = new DateTime(2025, 1, 2);
+            _mockHolidayRepository.Setup(repo => repo.GetHolidaysByDateAsync(date))
+                                  .ReturnsAsync(new List<Holiday>());
+
+            _mockEnricoConnector.Setup(connector => connector.WhereIsPublicHolidayAsync(date))
+                                .ReturnsAsync(new List<CountryModel>());
+
+            var result = await _holidayService.GetDateStatusAsync(date);
+
+            Assert.Equal(DayStatus.WorkDay, result);
+        }
+
+        [Theory]
+        [InlineData("2025-02-15", true)] // Saturday
+        [InlineData("2025-02-16", true)] // Sunday
+        [InlineData("2025-02-17", false)] // Monday
+        [InlineData("2025-02-13", false)] // Thursday
+        [InlineData("2025-02-14", false)] // Friday
+        public void DateIsFreeDay_ReturnsExpectedResult(string dateString, bool expected)
+        {
+            // Arrange
+            DateTime date = DateTime.Parse(dateString);
+
+            // Act
+            bool result = _holidayService.DateIsFreeDay(date);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ HolidaysAPI.Tests/HolidaysControllerSmokeTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+
+namespace HolidaysAPI.Tests
+{
+    public class HolidaysControllerSmokeTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public HolidaysControllerSmokeTests(WebApplicationFactory<Program> factory)
+        {
+            Environment.SetEnvironmentVariable("ConnectionStrings:HolidaysDBConnection", "Server=localhost,1433;Database=HolidaysDB_Test;User Id=sa;Password=Str0ngP@ssw0rd!;Integrated Security=True;TrustServerCertificate=True;Trusted_Connection = false;");
+            _factory = factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureServices(services =>
+                { });
+            });
+        }
+
+        [Theory]
+        [InlineData("/api/holidays/grouped-by-month?countryCode=US&year=2025")]
+        [InlineData("/api/holidays/get-date-status?date=2025-01-01")]
+        [InlineData("/api/holidays/get-max-consecutive-free-days?countryCode=US&year=2025")]
+        public async Task Get_EndpointsReturnSuccessAndJsonContentType(string url)
+        {
+            var client = _factory.CreateClient();
+
+            var response = await client.GetAsync(url);
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+    }
+}
